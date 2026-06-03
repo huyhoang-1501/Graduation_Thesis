d8fe8fd6e3202f64c3cd73e857d4bde7fed646ff
Nguyen Pham Huy Hoang <nguyenphamhuyhoang1501@gmail.com>
2026-06-03 11:24:59 +0700

    Add source code APP

---
+++ APP/source/app/src/main/java/app/web/graduation_thesis_3a3df/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.web.graduation_thesis_3a3df.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
